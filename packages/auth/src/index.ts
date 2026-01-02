21310b61f090743ffec0ab0c1d9e12a6a9fe197d
riolly <alberto.cahyantara@gmail.com>
2026-01-02 21:32:35 +0700

    fix: missing redirectURI for auth proxy

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -34,6 +34,7 @@ export function initAuth<
       google: {
         clientId: options.googleClientId,
         clientSecret: options.googleClientSecret,
+        redirectURI: `${options.baseUrl}/api/auth/callback/google`,
       },
     },
     trustedOrigins: ["expo://"],
